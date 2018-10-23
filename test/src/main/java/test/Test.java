0d0bdc9d7ba71eb788ebf6e9ef5af55157a258ad
ruiboot <466072539@qq.com>
2018-10-23 16:55:12 +0800

    文件中添加若干内容

--- test/src/main/java/test/Test.java
+++ test/src/main/java/test/Test.java
@@ -1,5 +1,16 @@
 package test;
 
 public class Test {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
